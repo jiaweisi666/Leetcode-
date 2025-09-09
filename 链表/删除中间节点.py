bfaed2144989677e3d50e564d895bbd7c2727ca9
yuanmeng <936853185@qq.com>
2025-09-09 15:12:26 +0800

    上传github

--- 链表/删除中间节点.py
+++ 链表/删除中间节点.py
@@ -3,8 +3,8 @@
 假定已知链表的某一个中间节点，请实现一种算法，将该节点从链表中删除。
 核心：非头尾节点
 """
+#解题思路：从当前节点向后溯源
 class Solution(object):
-    """解题思路：只能从当前节点向后溯源"""
     def deleteNode(self,Node):
         Node.val = Node.next.val
         Node.next = Node.next.next

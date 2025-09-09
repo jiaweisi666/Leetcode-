fe2ca6a745a881c7a387cf3b355b2bba79896fc5
yuanmeng <936853185@qq.com>
2025-09-09 15:04:29 +0800

    上传github

---
+++ 链表/删除中间节点.py
@@ -0,0 +1,9 @@
+"""
+题目描述：若链表中的某个节点，既不是链表头节点，也不是链表尾节点，则称其为该链表的「中间节点」。
+假定已知链表的某一个中间节点，请实现一种算法，将该节点从链表中删除。
+核心：非头尾节点
+"""
+class Solution(object):
+    def deleteNode(self,Node):
+        Node.val = Node.next.val
+        Node.next = Node.next.next

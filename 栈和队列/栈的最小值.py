f07e6cb0d2a2971fbe748c4fde7bf9a8bfcaf744
yuanmeng <936853185@qq.com>
2025-09-18 17:25:22 +0800

    上传github

--- 栈和队列/栈的最小值.py
+++ 栈和队列/栈的最小值.py
@@ -4,29 +4,46 @@ class MinStack:
     还支持min函数，该函数返回栈元素中的最小值。执行push、pop和min操作的时间复杂度必须为O(1)。
     """
     def __init__(self):
-        self.stack = [] #创建栈
-        self.stack_index = 0 #栈顶指针
+        self.stack = [] #创建主栈
+        self.min_stack = [] #创建辅助栈，它的唯一任务就是在任何时刻，它的栈顶元素都保存着主栈当前状态下的最小值
 
     def push(self, x: int) -> None:
         """
         将元素压入栈顶
         """
-        self.stack[stack_index] = x
-        self.stack_index += 1
+        self.stack.append(x)
+        if not self.min_stack:
+            self.min_stack.append(x)
+        else:
+            if x <= self.min_stack[-1]:
+                self.min_stack.append(x)
+            else:
+                self.min_stack.append(self.min_stack[-1])
 
     def pop(self) -> None:
         """
         弹出栈顶元素
         """
-        self.stack_index -= 1
-        return self.stack[self.stack_index]
-
+        if self.stack:
+            self.stack.pop()
+            self.min_stack.pop()
 
     def min(self) -> int:
-
+        return self.getMin()
 
 
     def top(self) -> int:
-
+        """
+        返回主栈的栈顶元素，不弹出。
+        """
+        if self.stack:
+            return self.stack[-1]
+        return None
 
     def getMin(self) -> int:
+        """
+        直接返回辅助栈栈顶元素
+        """
+        if self.min_stack:
+            return self.min_stack[-1]
+        return None
